409d8654d49d8d239608200db2d3fa51ea7cc655
nquynh06 <truongnhuquynh2k6@gmail.com>
2025-03-23 20:41:34 +0700

    Update appwriteConfig.ts

--- Frontend/src/PersonalKanban/appwriteConfig.ts
+++ Frontend/src/PersonalKanban/appwriteConfig.ts
@@ -1,26 +1,22 @@
-// src/api/appwrite.ts
 import { Client, Databases } from "appwrite";
 
-// Khởi tạo Appwrite Client
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1") // Đổi thành endpoint của bạn
   .setProject("67506d930039e1733c08");
 
 const databases = new Databases(client);
 
-// Định nghĩa kiểu dữ liệu của Subject
 export interface Subject {
   $id: string;
   $collectionId: string;
   $databaseId: string;
   $createdAt: string;
   $updatedAt: string;
-  $permissions: string[];  // Thêm dòng này
+  $permissions: string[];  
   title: string;
 }
 
 
-// Hàm lấy danh sách môn học từ Appwrite
 export const fetchSubjects = async (): Promise<Subject[]> => {
   try {
     const response = await databases.listDocuments<Subject>(
