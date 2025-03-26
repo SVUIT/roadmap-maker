87de2d3eaf73396d4c6911bb13fcaa0e2a76972a
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-03-26 15:25:18 +0000

    fixing bugs for reactjs and typescript

--- Frontend/src/reportWebVitals.ts
+++ Frontend/src/reportWebVitals.ts
@@ -1,14 +1,12 @@
-import { ReportHandler } from 'web-vitals';
-
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+import {onCLS,onFID,onFCP,onINP,onLCP,onTTFB} from 'web-vitals'
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);  
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    onCLS(onPerfEntry);
+    onFID(onPerfEntry);
+    onFCP(onPerfEntry);
+    onINP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 }
 
